e05a1fafcaa07dddf99691b79935810859035731
HoaLyNg <lynguyenthanhhoaioi@gmail.com>
2019-04-16 00:00:30 +0700

    Test load table

---
+++ TestTableManager/UnitTestManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuanLyNhaHang;
+using QuanLyNhaHang.DAO;
+
+namespace TestTableManager
+{
+    [TestClass]
+    public class UnitTestManager
+    {
+        [TestMethod]
+        public void TestGetInforTable()
+        {
+            int id = 2;
+            int count = 0;
+            int count_expect = 0;
+            List<QuanLyNhaHang.DTO.Menu> listBillInfo = MenuDAO.Instance.GetListMenuByTable(id);
+            float totalPrice = 0;
+            foreach (QuanLyNhaHang.DTO.Menu item in listBillInfo)
+            {
+                count++;
+            }
+            Assert.AreEqual(count_expect, count);
+        }
+
+        [TestMethod]
+        public void TestGetInforTable1()
+        {
+            int id = 6;
+            int count = 0;
+            int count_expect = 1;
+            List<QuanLyNhaHang.DTO.Menu> listBillInfo = MenuDAO.Instance.GetListMenuByTable(id);
+            float totalPrice = 0;
+            foreach (QuanLyNhaHang.DTO.Menu item in listBillInfo)
+            {
+                count++;
+            }
+            Assert.AreEqual(count_expect, count);
+        }
+    }
+}
